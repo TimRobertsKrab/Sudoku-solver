2198c46f6a7e9388f42e033104c473b876ad80fc
TimRobertsKrab <troberts95@outlook.com>
2024-11-08 01:09:23 +0000

    Changes file names and some functions to be more descriptive

---
+++ Solver.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver {
+    static class Solver {
+        const int MAX_ITERATIONS = 1000000;
+        static int iterations = 0;
+        public static bool BruteForce(Cell[,] cells ,int y, int x){
+            if (iterations >= MAX_ITERATIONS){
+                return false;
+            }
+            int newx = x+1;
+            int newy = y;
+            if (newx>8){
+                newx = 0;
+                newy++;
+            }
+
+            if (!cells[y,x].GetIsFixed()){
+                cells[y,x].SetValue(1);
+            }
+            else{
+                if (newy > 8){
+                    return true;
+                }
+                return BruteForce(cells,newy,newx);
+            }
+            
+            while (cells[y,x].GetValue() <= 9){
+                if (!ViolationCheck(cells,y,x)){
+                    
+                    if (newy > 8){
+                        return true;
+                    }
+                    
+                    if (BruteForce(cells,newy,newx)){
+                        return true;
+                    }
+                }
+                cells[y,x].IncrementValue();
+            }
+            ++iterations;
+            cells[y,x].SetValue(0);
+            return false;
+        } 
+
+        public static bool ViolationCheck(Cell[,] cells,int y, int x) {
+            byte value = cells[y,x].GetValue();
+            //Check for row violation
+            for (int j = 0; j < 9; j++) {
+                if (j == x) {
+                    continue;
+                }
+                if (cells[y,j].GetValue() == value) {
+                    return true;
+                }
+            }
+
+            //Check for column violation
+            for (int i = 0; i < 9; i++) {
+                if (i == y) {
+                    continue;
+                }
+                if (cells[i, x].GetValue() == value) {
+                    return true;
+                }
+            }
+
+            //Check for square violation
+            for (int i = (y/3) * 3, ylen = i + 3;i < ylen; i++) {
+                for(int j = (x/3)*3,xlen = j + 3; j < xlen; j++) {
+                    // Ignore the same column or row as value as it was already checked
+                    if (i == y || j == x) {
+                        continue;
+                    }
+                    if (cells[i,j].GetValue() == value) {
+                        return true;                   
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuSolver {
+    public partial class Window : Form {
+
+        private Cell[,] cells;
+        private List<System.Windows.Forms.TextBox> textBoxes = new List<System.Windows.Forms.TextBox>();
+        
+        public Window() {
+            InitializeComponent();
+            textBoxes.Add(textBox1); textBoxes.Add(textBox2); textBoxes.Add(textBox3); textBoxes.Add(textBox4);
+            textBoxes.Add(textBox5); textBoxes.Add(textBox6); textBoxes.Add(textBox7); textBoxes.Add(textBox8);
+            textBoxes.Add(textBox9); textBoxes.Add(textBox10); textBoxes.Add(textBox11); textBoxes.Add(textBox12);
+            textBoxes.Add(textBox13); textBoxes.Add(textBox14); textBoxes.Add(textBox15); textBoxes.Add(textBox16);
+            textBoxes.Add(textBox17); textBoxes.Add(textBox18); textBoxes.Add(textBox19); textBoxes.Add(textBox20);
+            textBoxes.Add(textBox21); textBoxes.Add(textBox22); textBoxes.Add(textBox23); textBoxes.Add(textBox24);
+            textBoxes.Add(textBox25); textBoxes.Add(textBox26); textBoxes.Add(textBox27); textBoxes.Add(textBox28);
+            textBoxes.Add(textBox29); textBoxes.Add(textBox30); textBoxes.Add(textBox31); textBoxes.Add(textBox32);
+            textBoxes.Add(textBox33); textBoxes.Add(textBox34); textBoxes.Add(textBox35); textBoxes.Add(textBox36);
+            textBoxes.Add(textBox37); textBoxes.Add(textBox38); textBoxes.Add(textBox39); textBoxes.Add(textBox40);
+            textBoxes.Add(textBox41); textBoxes.Add(textBox42); textBoxes.Add(textBox43); textBoxes.Add(textBox44);
+            textBoxes.Add(textBox45); textBoxes.Add(textBox46); textBoxes.Add(textBox47); textBoxes.Add(textBox48);
+            textBoxes.Add(textBox49); textBoxes.Add(textBox50); textBoxes.Add(textBox51); textBoxes.Add(textBox52);
+            textBoxes.Add(textBox53); textBoxes.Add(textBox54); textBoxes.Add(textBox55); textBoxes.Add(textBox56);
+            textBoxes.Add(textBox57); textBoxes.Add(textBox58); textBoxes.Add(textBox59); textBoxes.Add(textBox60);
+            textBoxes.Add(textBox61); textBoxes.Add(textBox62); textBoxes.Add(textBox63); textBoxes.Add(textBox64);
+            textBoxes.Add(textBox65); textBoxes.Add(textBox66); textBoxes.Add(textBox67); textBoxes.Add(textBox68);
+            textBoxes.Add(textBox69); textBoxes.Add(textBox70); textBoxes.Add(textBox71); textBoxes.Add(textBox72);
+            textBoxes.Add(textBox73); textBoxes.Add(textBox74); textBoxes.Add(textBox75); textBoxes.Add(textBox76);
+            textBoxes.Add(textBox77); textBoxes.Add(textBox78); textBoxes.Add(textBox79); textBoxes.Add(textBox80);
+            textBoxes.Add(textBox81);
+        }
+
+        private bool InitialiseCells() {
+            cells = new Cell[9, 9];
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (textBoxes[i * 9 + j].Text == "") {
+                        cells[i, j] = new Cell(0, false);
+                    }
+                    else {
+                        cells[i, j] = new Cell(Convert.ToByte(textBoxes[i * 9 + j].Text), true);
+                    }
+                }
+            }
+            for (int i = 0; i < 9; i++){
+                for (int j = 0; j < 9; j++){
+                    if (cells[i,j].GetIsFixed()){
+                        if (Solver.ViolationCheck(cells,i,j)){
+                            textBoxes[i*9+j].BackColor = Color.Red;
+                            return false;
+                        }
+                    }
+                }
+
+            }
+            return true;
+        }
+        
+        private void SolveButton_Click(object sender, EventArgs e) { 
+            if (!InitialiseCells()){
+                return;
+            }
+            if (Solver.BruteForce(cells,0,0)){
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        if (!cells[i, j].GetIsFixed()) {
+                            textBoxes[i * 9 + j].Text = Convert.ToString(cells[i, j].GetValue());
+                        }
+                        textBoxes[i * 9 + j].Enabled = false;
+                    }
+                }
+            }
+            else{
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        textBoxes[i * 9 + j].Text = "0";
+                        textBoxes[i * 9 + j].Enabled = false;
+                    }
+                }
+            }
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e) {
+            for (int i = 0; i < 81; i++) {
+                textBoxes[i].Enabled = true;
+                textBoxes[i].Text = "";
+                textBoxes[i].BackColor = Color.White;
+            }
+        }
+    }
+}
